8e5f308f12515fa924d95653dfef5dd9f06cea5d
Renan Garcia <48793704+RenanGarcia1@users.noreply.github.com>
2021-10-26 20:22:57 -0300

    Update banco.php

--- model/banco.php
+++ model/banco.php
@@ -6,7 +6,7 @@ date_default_timezone_set('America/Sao_Paulo');
 define('BD_SERVIDOR','localhost');
 define('BD_USUARIO','root');
 define('BD_SENHA','');
-define('BD_BANCO','projetoweb');
+define('BD_BANCO','projeto_web');
     
 class Banco{
 
